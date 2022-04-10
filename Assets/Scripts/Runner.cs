509ebfbb027e10a25ea30d2e900e755803e027de
Byron Crowhurst <57679765+ByPossum@users.noreply.github.com>
2022-04-10 19:11:01 +0100

    Text id read from unity text box

--- Assets/Scripts/Runner.cs
+++ Assets/Scripts/Runner.cs
@@ -10,11 +10,16 @@ public class Runner : MonoBehaviour
     [SerializeField] Text langRef;
     [SerializeField] RectTransform rectToDrawTo;
     [SerializeField] TextDisplayer td;
+    private Text t_textToDisplay;
+    private string s_dialogue = "";
     // Start is called before the first frame update
     void Start()
     {
         FindObjectOfType<TextManager>().Init();
         StartCoroutine(GetTextDelayed());
+        t_textToDisplay = rectToDrawTo.gameObject.GetComponent<Text>();
+        s_dialogue = t_textToDisplay?.text;
+        td.DisplayText(TextManager.x.GetText(s_dialogue), rectToDrawTo, TextDisplayType.character);
     }
 
     private void Update()
@@ -22,22 +27,22 @@ public class Runner : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.W))
         {
             TextManager.x.l_currentLanguage = Languages.eng;
-            td.DisplayText(TextManager.x.GetText("tut01"), rectToDrawTo, TextDisplayType.character);
+            td.DisplayText(TextManager.x.GetText(s_dialogue), rectToDrawTo, TextDisplayType.character);
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
             TextManager.x.l_currentLanguage = Languages.isl;
-            td.DisplayText(TextManager.x.GetText("tut01"), rectToDrawTo, TextDisplayType.character);
+            td.DisplayText(TextManager.x.GetText(s_dialogue), rectToDrawTo, TextDisplayType.character);
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
             TextManager.x.l_currentLanguage = Languages.eng;
-            td.DisplayText(TextManager.x.GetText("tut02"), rectToDrawTo, TextDisplayType.character);
+            td.DisplayText(TextManager.x.GetText(s_dialogue), rectToDrawTo, TextDisplayType.character);
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
             TextManager.x.l_currentLanguage = Languages.isl;
-            td.DisplayText(TextManager.x.GetText("tut02"), rectToDrawTo, TextDisplayType.character);
+            td.DisplayText(TextManager.x.GetText(s_dialogue), rectToDrawTo, TextDisplayType.character);
         }
         langRef.text = TextManager.x.l_currentLanguage.ToString();
     }
@@ -45,6 +50,6 @@ public class Runner : MonoBehaviour
     private IEnumerator GetTextDelayed()
     {
         yield return new WaitForSeconds(0.0f);
-        Debug.Log(TextManager.x.GetText("tut01"));
+        Debug.Log(TextManager.x.GetText(s_dialogue));
     }
 }
