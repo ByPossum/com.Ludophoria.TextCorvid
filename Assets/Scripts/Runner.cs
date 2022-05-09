c0a3a77237bddfd70cb447f12d26f6d7f087b1e5
Byron Crowhurst <57679765+ByPossum@users.noreply.github.com>
2022-05-09 14:36:46 +0100

    Revert "Merge pull request #1 from ByPossum/com.Ludophoria.TextCorvid"
    This reverts commit 2c2b2697e30d4dddefe886955ac0ed8257e2a7a7, reversing
    changes made to e06b33e2974441c6f86f22607207f0938abd4346.

---
+++ Assets/Scripts/Runner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TextCorvid;
+using UnityEngine.UI;
+
+public class Runner : MonoBehaviour
+{
+    [SerializeField] Text textRef;
+    [SerializeField] Text langRef;
+    [SerializeField] RectTransform rectToDrawTo;
+    [SerializeField] TextDisplayer td;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<TextManager>().Init();
+        StartCoroutine(GetTextDelayed());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            TextManager.x.l_currentLanguage = Languages.eng;
+            td.DisplayText(TextManager.x.GetText("tut01"), rectToDrawTo, TextDisplayType.character);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            TextManager.x.l_currentLanguage = Languages.isl;
+            td.DisplayText(TextManager.x.GetText("tut01"), rectToDrawTo, TextDisplayType.character);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            TextManager.x.l_currentLanguage = Languages.eng;
+            td.DisplayText(TextManager.x.GetText("tut02"), rectToDrawTo, TextDisplayType.character);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            TextManager.x.l_currentLanguage = Languages.isl;
+            td.DisplayText(TextManager.x.GetText("tut02"), rectToDrawTo, TextDisplayType.character);
+        }
+        langRef.text = TextManager.x.l_currentLanguage.ToString();
+    }
+
+    private IEnumerator GetTextDelayed()
+    {
+        yield return new WaitForSeconds(0.0f);
+        Debug.Log(TextManager.x.GetText("tut01"));
+    }
+}
---
+++ Assets/Scripts/TextDisplayer.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TextCorvid
+{
+    public class TextDisplayer : MonoBehaviour
+    {
+        [SerializeField] private RectTransform rt_displayBox;
+        public List<string> textKeys = new List<string>();
+        public Text refText;
+        List<Text> previousText = new List<Text>();
+        private int prevLineCount;
+        private int currentRow;
+        private int rowCount;
+        public bool b_done = true;
+
+        /// <summary>
+        /// Choose how the text will be displayed, and on which rect transform.
+        /// </summary>
+        /// <param name="textToDisplay">String to display (typically gotten from TextManager.x.GetText()</param>
+        /// <param name="rectToDisplay">Rect Transform used to display on. Will display on the top left of the rect transform.</param>
+        /// <param name="displayType">How the text will be displayed.</param>
+        public void DisplayText(string textToDisplay, RectTransform rectToDisplay, TextDisplayType displayType)
+        {
+            switch (displayType)
+            {
+                case TextDisplayType.block:
+                    break;
+                case TextDisplayType.line:
+                    break;
+                case TextDisplayType.word:
+                    break;
+                case TextDisplayType.character:
+                    DisplayByChar(textToDisplay, rectToDisplay);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Choose how the text will be displayed. Will be displayed on serialized rect transform.
+        /// </summary>
+        /// <param name="textToDisplay">String to display (typically gotten from TextManager.x.GetText()</param>
+        /// <param name="displayType">How the text will be displayed</param>
+        public void DisplayText(string textToDisplay, TextDisplayType displayType)
+        {
+            switch (displayType)
+            {
+                case TextDisplayType.none:
+                    break;
+                case TextDisplayType.block:
+                    break;
+                case TextDisplayType.line:
+                    break;
+                case TextDisplayType.word:
+                    break;
+                case TextDisplayType.character:
+                    try
+                    {
+                        DisplayByChar(textToDisplay, rt_displayBox);
+                    }
+                    catch(System.NullReferenceException nre)
+                    {
+                        Debug.LogException(nre);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Display the text character by character.
+        /// </summary>
+        /// <param name="textToDisplay">Characters to display.</param>
+        /// <param name="rectToDisplay">Where to begin displaying those characters.</param>
+        private void DisplayByChar(string textToDisplay, RectTransform rectToDisplay)
+        {
+            b_done = false;
+            currentRow = 0;
+            prevLineCount = 0;
+            rowCount = Mathf.FloorToInt(rectToDisplay.rect.height / refText.preferredHeight);
+            string[] _words = textToDisplay.Split(' ');
+            DeleteOldText();
+            StartCoroutine(ShowNextCharacterByCharacter(textToDisplay, rectToDisplay));
+            b_done = true;
+        }
+        
+        /// <summary>
+        /// Remove old text from the rect.
+        /// </summary>
+        private void DeleteOldText()
+        {
+            foreach(Text killMe in previousText)
+            {
+                DestroyImmediate(killMe.gameObject);
+            }
+            previousText = new List<Text>();
+        }
+
+        /// <summary>
+        /// Displays a character, then waits for "text speed" until all characters are displayed.
+        /// </summary>
+        /// <param name="nextString">The string to display. Will accomidate for the text being larger than the rect.</param>
+        /// <param name="_parent">The area to display the text.</param>
+        /// <returns>Waits for text speed.</returns>
+        private IEnumerator ShowNextCharacterByCharacter(string nextString, RectTransform _parent)
+        {
+            // Show the text character by character
+            for(int i = 0; i < nextString.Length; i++)
+            {
+                // Move to a new row when the next word goes over the textbox width
+                if ((prevLineCount * refText.fontSize) + (refText.fontSize * nextString.Length) > _parent.rect.width)
+                {
+                    currentRow++;
+                    prevLineCount = 0;
+                }
+                Text newText = Instantiate<Text>(refText);
+                newText.text = nextString[i].ToString();
+                newText.transform.parent = _parent;
+                // Set the top left of the text to the top left of the box with character offsets (Not fully working yet)
+                newText.rectTransform.offsetMin = new Vector2((_parent.offsetMin.x + refText.fontSize * 3) + prevLineCount * refText.fontSize, ((_parent.offsetMax.x - _parent.offsetMax.x) - refText.fontSize * 3) - refText.fontSize * (currentRow));
+                //newText.rectTransform.offsetMax = new Vector2(_parent.offsetMax.x, (_parent.offsetMax.y + refText.fontSize) + refText.fontSize * (currentRow));
+                newText.rectTransform.sizeDelta = new Vector2(100, 100);
+                previousText.Add(newText);
+                prevLineCount++;
+                yield return new WaitForSeconds(TextManager.x.f_textSpeed);
+            }
+        }
+    }
+    
+    public enum TextDisplayType
+    {
+        none,       // Don't display
+        block,      // Display all text at once
+        line,       // Display line by line
+        word,       // Display word by word
+        character   // Display character by character
+    }
+
+}
