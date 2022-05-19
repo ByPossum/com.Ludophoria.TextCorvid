730fc8a47e81d8f9039e938eb436145483f7a8af
Byron Crowhurst <57679765+ByPossum@users.noreply.github.com>
2022-05-19 16:20:45 +0100

    Singleton Fix

--- Runtime/Scripts/Runner.cs
+++ Runtime/Scripts/Runner.cs
@@ -11,6 +11,7 @@ public class Runner : MonoBehaviour
     [SerializeField] TMP_Text langRef;
     [SerializeField] RectTransform rectToDrawTo;
     [SerializeField] TextDisplayer td;
+    private TextManager tm_managerRef;
     private TMP_Text t_textToDisplay;
     private string s_dialogue = "";
     // Start is called before the first frame update
@@ -19,29 +20,34 @@ public class Runner : MonoBehaviour
         FindObjectOfType<TextManager>().Init();
         t_textToDisplay = rectToDrawTo.gameObject.GetComponent<TMP_Text>();
         s_dialogue = t_textToDisplay?.text;
-        td.Init(TextManager.x.f_textSpeed);
-        td.DisplayText(TextManager.x.GetText(s_dialogue), rectToDrawTo, TextDisplayType.character);
-        TextManager.x.l_currentLanguage = Languages.eng;
+#if ALLOW_SINGLETON
+        tm_managerRef = TextManager.x;
+#else
+        tm_managerRef = FindObjectOfType<TextManager>();
+#endif
+        td.Init(tm_managerRef.f_textSpeed);
+        td.DisplayText(tm_managerRef.GetText(s_dialogue), rectToDrawTo, TextDisplayType.character);
+        tm_managerRef.l_currentLanguage = Languages.eng;
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            td.DisplayText(TextManager.x.GetText("tut01"), rectToDrawTo, TextDisplayType.character);
+            td.DisplayText(tm_managerRef.GetText("tut01"), rectToDrawTo, TextDisplayType.character);
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
-            td.DisplayText(TextManager.x.GetText("test02"), rectToDrawTo, TextDisplayType.character);
+            td.DisplayText(tm_managerRef.GetText("test02"), rectToDrawTo, TextDisplayType.character);
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
-            td.DisplayText(TextManager.x.GetText("test03"), rectToDrawTo, TextDisplayType.character);
+            td.DisplayText(tm_managerRef.GetText("test03"), rectToDrawTo, TextDisplayType.character);
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
-            td.DisplayText(TextManager.x.GetText("test04"), rectToDrawTo, TextDisplayType.character);
+            td.DisplayText(tm_managerRef.GetText("test04"), rectToDrawTo, TextDisplayType.character);
         }
-        langRef.text = TextManager.x.l_currentLanguage.ToString();
+        langRef.text = tm_managerRef.l_currentLanguage.ToString();
     }
 }
