e0949aec231fa68d640123db0290a35a5ec1920b
BYRON CROWHURST <57679765+ByPossum@users.noreply.github.com>
2022-06-21 16:48:58 +0100

    Update CreateTCPrefabs.cs

--- Editor/CreateTCPrefabs.cs
+++ Editor/CreateTCPrefabs.cs
@@ -8,11 +8,16 @@ public class CreateTCPrefabs
     [MenuItem("GameObject/TextCorvid/Resizable Textbox", false, 1)]
     static void CreateResizableTextBox()
     {
-        GameObject _obj = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/com.Ludophoria.TextCorvid/Runtime/Prefabs/ResizableTextBox.prefab", typeof(GameObject));
-        
-        if(!_obj)
-            Debug.LogError("ResizableTextBox prefab Not Found");
-        
+        GameObject _obj = (GameObject)AssetDatabase.LoadAssetAtPath("Packages/Text Corvid/Runtime/Prefabs/ResizableTextBox.prefab", typeof(GameObject));
+
+        if (!_obj)
+        {
+
+            _obj = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/com.Ludophoria.TextCorvid/Runtime/Prefabs/ResizableTextBox.prefab", typeof(GameObject));
+            if(!_obj)
+                Debug.LogError("ResizableTextBox prefab Not Found");
+        }
+
         GameObject go = PrefabUtility.InstantiatePrefab(_obj, Selection.activeTransform) as GameObject;
     }
 }
