2a8793743b0ec4d6f5212429d0f1fe0665d2a500
BYRON CROWHURST <57679765+ByPossum@users.noreply.github.com>
2022-06-28 11:28:26 +0100

    Trigger Text boxes added

--- Editor/CreateTCPrefabs.cs
+++ Editor/CreateTCPrefabs.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 
 public class CreateTCPrefabs
 {
-    [MenuItem("GameObject/TextCorvid/Resizable Textbox", false, 1)]
+    [MenuItem("GameObject/TextCorvid/Resizable Textbox", false, 4)]
     static void CreateResizableTextBox()
     {
         CreateNewObjectFromString("com.Ludophoria.TextCorvid/Runtime/Prefabs/ResizableTextBox.prefab");
     }
 
-    [MenuItem("GameObject/TextCorvid/World Space Textbox", false, 1)]
+    [MenuItem("GameObject/TextCorvid/World Space Textbox", false, 2)]
     static void CreateWSTextBox()
     {
         CreateNewObjectFromString("com.Ludophoria.TextCorvid/Runtime/Prefabs/WSCanvasTextBox.prefab");
@@ -22,6 +22,12 @@ public class CreateTCPrefabs
     {
         CreateNewObjectFromString("com.Ludophoria.TextCorvid/Runtime/Prefabs/TextManager.prefab");
     }
+    [MenuItem("GameObject/TextCorvid/Trigger Text Box", false, 3)]
+    static void CreateTriggerTextBox()
+    {
+        CreateNewObjectFromString("com.Ludophoria.TextCorvid/Runtime/Prefabs/TriggerTextBox.prefab");
+    }
+
     static void CreateNewObjectFromString(string filePath)
     {
         GameObject _obj = (GameObject)AssetDatabase.LoadAssetAtPath("Packages/" + filePath, typeof(GameObject));
