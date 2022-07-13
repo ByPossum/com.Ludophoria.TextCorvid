34ff22bbfe52fec27440beab9f1d5afc4ec93870
BYRON CROWHURST <57679765+ByPossum@users.noreply.github.com>
2022-07-13 16:50:18 +0100

    Put in the groundwork for resizable textbox v2

--- Assets/com.Ludophoria.TextCorvid/Runtime/Scripts/ResizableTextBox.cs
+++ Assets/com.Ludophoria.TextCorvid/Runtime/Scripts/ResizableTextBox.cs
@@ -16,6 +16,7 @@ namespace TextCorvid
         private RectTransform rt_box;
         private SpriteRenderer sr_textBox;
         private bool b_boxSizeSatisfied = false;
+        private float f_minWidth, f_maxWidth, f_minHeight, f_maxHeight;
 
         public int Width { get { return i_currentWidth; } }
         public int Height { get { return i_currentHeight; } }
@@ -45,6 +46,7 @@ namespace TextCorvid
             sr_textBox = GetComponent<SpriteRenderer>();
             rt_box = GetComponent<RectTransform>();
             List<string> _bins = CollectTextIntoBins(i_minCharactersPerLine, _text);
+            List<string> _dummy = CollectTextIntoBins(i_minCharactersPerLine, _text, _container);
             Vector2 _resize = ResizeBoxV2(_text, _container, _bins);
             f_currentBoxWidth = _resize.x;
             f_currentBoxHeight = _resize.y;
@@ -70,8 +72,12 @@ namespace TextCorvid
             // return previous guess when both hg and wg are too small
             // if width can go lower
             //      Set Largest Width to Width Guess
+            // if width is too low
+            //      Set Lowest Width to Width Guess
             // if height can go lower
             //      Set Largest Height to Height Guess
+            // if height is too low
+            //      Set Lowest Height to Height Guess
             // Solve again
             // Until you can't go smaller
 
@@ -96,6 +102,29 @@ namespace TextCorvid
             return new Vector2(_previousWidth, _previousHeight * _fontSize);
         }
     
+        private List<string> CollectTextIntoBins(int _binLength, string _text, TMP_Text _box)
+        {
+            _box.text = _text;
+            int linierIter = 0;
+            List<string> _binsOfText = new List<string>();
+            string[] words = _text.Split(' ');
+            string _nextBin = string.Empty;
+            foreach (string word in words)
+            {
+                int tmp = 0;
+                int j = 0;
+                for(int i = linierIter; i < linierIter + word.Length; i++)
+                {
+                    int _characterIndex = GetCharacterIndex(word[j], _box.textInfo.characterInfo);
+                    Debug.Log($"Word Length {word.Length} | Character {_characterIndex} Width {_box.textInfo.characterInfo[_characterIndex].character}");
+                    j++;
+                    tmp = linierIter + i;
+                }
+                linierIter = tmp + 1;
+            }
+            return _binsOfText;
+        }
+
         private List<string> CollectTextIntoBins(int _binLength, string _text)
         {
             b_boxSizeSatisfied = true;
@@ -121,5 +150,15 @@ namespace TextCorvid
                 _binsOfText.Add(_nextBin.Remove(_nextBin.Length - 1));
             return _binsOfText;
         }
+        private int GetCharacterIndex(char _character, TMP_CharacterInfo[] _info)
+        {
+            // All characters in this Character info are empty...
+            // Unity throw me a bone here please for the love of god.
+            for (int i = 0; i < _info.Length; i++)
+                if (_character == _info[i].character)
+                    return i;
+            return 0;
+        }
     }
+
 }
