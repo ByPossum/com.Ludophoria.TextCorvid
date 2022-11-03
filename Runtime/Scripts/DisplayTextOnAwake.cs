45bb927a2f2b603596165c5dd765270ad5f99e27
Byron Crowhurst <57679765+ByPossum@users.noreply.github.com>
2022-11-03 16:52:26 +0000

    All text display types implemented

--- Runtime/Scripts/DisplayTextOnAwake.cs
+++ Runtime/Scripts/DisplayTextOnAwake.cs
@@ -9,6 +9,7 @@ namespace TextCorvid
 
         TextGlue tg;
         TextDisplayer td_displayer;
+        [SerializeField] private TextDisplayType tdt_displayType;
         public async void Awake()
         {
             // This is a really horrid way of trying to avoid race conditions but I'm not above programming like a dingus so deal with it.
@@ -18,14 +19,15 @@ namespace TextCorvid
                 await System.Threading.Tasks.Task.Yield();
             }
 
+
             td_displayer = GetComponentInChildren<TextDisplayer>(true);
             td_displayer.Init(tg.GetTextManager().TextSpeed, tg.GetTextAnimator());
             // NB: Change text display type to some variable?
             ResizableTextBox tb = GetComponentInChildren<ResizableTextBox>();
             if (tb)
-                td_displayer.DisplayText(tg.GetTextManager().GetText(td_displayer.GetComponentInChildren<TMPro.TMP_Text>(true).text), tb, TextDisplayType.character);
+                td_displayer.DisplayText(tg.GetTextManager().GetText(td_displayer.TextID), tb, tdt_displayType);
             else
-                td_displayer.DisplayText(tg.GetTextManager().GetText(td_displayer.GetComponentInChildren<TMPro.TMP_Text>(true).text), GetComponent<RectTransform>().rect.height, TextDisplayType.character);
+                td_displayer.DisplayText(tg.GetTextManager().GetText(td_displayer.TextID), GetComponent<RectTransform>().rect.height, tdt_displayType);
         }
     }
 }
