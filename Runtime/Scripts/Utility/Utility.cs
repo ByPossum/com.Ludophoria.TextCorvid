c595441e88848f687cb8d4730337535e026e3f50
Byron Crowhurst <57679765+ByPossum@users.noreply.github.com>
2023-06-23 10:35:21 +0100

    Update Utility.cs

--- Runtime/Scripts/Utility/Utility.cs
+++ Runtime/Scripts/Utility/Utility.cs
@@ -1,21 +1,25 @@
-public static class Utility
+namespace TextCorvid
 {
-    /// <summary>
-    /// Sets the array to a new size and reads as many of the old items into the new array as possible.
-    /// </summary>
-    /// <typeparam name="T">The type of Array you wish to resize</typeparam>
-    /// <param name="_oldArray">The old Array you wish to resize</param>
-    /// <param name="_newSize">The size for the new Array</param>
-    /// <returns>A new Array with as many of the old items in it as was allowed.</returns>
-    public static T[] ResizeArray<T>(T[] _oldArray, int _newSize)
+    public static class Utility
     {
-        T[] _newArray = new T[_newSize];
-        for (int i = 0; i < _newSize; i++)
+        /// <summary>
+        /// Sets the array to a new size and reads as many of the old items into the new array as possible.
+        /// </summary>
+        /// <typeparam name="T">The type of Array you wish to resize</typeparam>
+        /// <param name="_oldArray">The old Array you wish to resize</param>
+        /// <param name="_newSize">The size for the new Array</param>
+        /// <returns>A new Array with as many of the old items in it as was allowed.</returns>
+        public static T[] ResizeArray<T>(T[] _oldArray, int _newSize)
         {
-            if (i > _oldArray.Length - 1)
-                return _newArray;
-            _newArray[i] = _oldArray[i];
+            T[] _newArray = new T[_newSize];
+            for (int i = 0; i < _newSize; i++)
+            {
+                if (i > _oldArray.Length - 1)
+                    return _newArray;
+                _newArray[i] = _oldArray[i];
+            }
+            return _newArray;
         }
-        return _newArray;
     }
+
 }
