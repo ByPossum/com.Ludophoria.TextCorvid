d1d7bb66b224551684b8f750f2cb6a994dd0efd3
Byron Crowhurst <57679765+ByPossum@users.noreply.github.com>
2023-08-07 13:27:22 +0100

    Can now load multiple text files.

--- Assets/com.Ludophoria.TextCorvid/Runtime/Scripts/Data/TextSettings.cs
+++ Assets/com.Ludophoria.TextCorvid/Runtime/Scripts/Data/TextSettings.cs
@@ -7,8 +7,8 @@ namespace TextCorvid
     [CreateAssetMenu(fileName ="TextSettings", menuName ="TextCorvid/New Text Settings", order = 69)]
     public class TextSettings : ScriptableObject
     {
-        public string s_filePath;
-        public string s_folderPaths;
+        public string[] s_filePath;
+        public string[] s_folderPaths;
         public Languages[] A_supportedLanguages;
         public int i_textSpeed;
         public bool b_autoDetectFile;
---
+++ Assets/com.Ludophoria.TextCorvid/Runtime/Scripts/Display/TextboxArrow.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextboxArrow : MonoBehaviour
+{
+    private Vector2 v_target;
+    public void CreateArrow(RectTransform parent, Vector2 target)
+    {
+
+    }
+}
