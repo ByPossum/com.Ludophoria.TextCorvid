cdd8b52a6c8e80a0614d696da8441b0683e0217c
Byron Crowhurst <57679765+ByPossum@users.noreply.github.com>
2023-09-22 14:02:07 +0100

    Mover now just uses parent transform pos if start or end is null

--- Assets/com.Ludophoria.TextCorvid/Runtime/Scripts/UnityIntegration/MoveScaler.cs
+++ Assets/com.Ludophoria.TextCorvid/Runtime/Scripts/UnityIntegration/MoveScaler.cs
@@ -12,15 +12,14 @@ namespace TextCorvid
         [SerializeField] private float f_duration;
         [SerializeField] private GameObject go_startPoint;
         [SerializeField] private GameObject go_endPoint;
+        [SerializeField] private Vector3 v_startPos;
+        [SerializeField] private Vector3 v_endPos;
 
         public override async void Begin()
         {
             if (a_animationController)
                 a_animationController?.SetBool("active", true);
-            if(!go_endPoint)
-                go_endPoint = transform.parent.gameObject;
-            if(!go_startPoint)
-                go_startPoint = transform.parent.gameObject;
+
             await Move(go_startPoint, go_endPoint);
         }
 
@@ -36,8 +35,8 @@ namespace TextCorvid
 
         private async Task Move(GameObject _start, GameObject _end)
         {
-            Vector3 _startPoint = Camera.main.WorldToScreenPoint(_start.transform.position);
-            Vector3 _endPoint = Camera.main.WorldToScreenPoint(_end.transform.position);
+            Vector3 _startPoint = _start ? Camera.main.WorldToScreenPoint(_start.transform.position) : transform.parent.position;
+            Vector3 _endPoint = _end ? Camera.main.WorldToScreenPoint(_end.transform.position) : transform.parent.position;
             for (float i = 0; i < 1; i += Time.deltaTime * (1 / f_duration))
             {
                 transform.parent.position = Vector3.Lerp(_startPoint, _endPoint, i);
